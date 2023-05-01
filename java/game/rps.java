c87de25ce69ce5459f6c64031baeb89386b30bd0
Abinesh <101916855+abinesh-code@users.noreply.github.com>
2023-05-01 12:02:41 +0530

    Update rps.java

--- java/game/rps.java
+++ java/game/rps.java
@@ -5,11 +5,11 @@ public class rps {
 		Random r=new Random();
 		Scanner sc=new Scanner(System.in);
 		
-		String arr[]={"rock","paper","sisor"};
+		String arr[]={"rock","paper","scissor"};
 		
 		String n1="rock";
 		String n2="paper";
-		String n3="sisor";
+		String n3="scissor";
 		
 		System.out.println("Type any one from this-->>"+" "+n1+" "+n2+" "+n3);
 		String m=sc.nextLine();
@@ -17,9 +17,14 @@ public class rps {
 		String q=arr[r.nextInt(3)];
 		if(m.equals(q))
 			System.out.println("Its a tie");
-		else if((m.equals("rock") && q.equals("sisor")) || (m.equals("paper") && q.equals("rock")) || (m.equals("sisor") && q.equals("paper")))
-			System.out.println("You win...!"+" machine choice is: "+q);
+		
+		else if((m.equals("rock") && q.equals("scissor")) || (m.equals("paper") && q.equals("rock")) || (m.equals("scissor") && q.equals("paper")))
+			System.out.println("You win...!"+ " machine choice is: "+q);
+		
+		else if((q.equals("rock") && m.equals("scissor")) || (q.equals("paper") && m.equals("rock")) || (q.equals("scissor") && m.equals("paper")))
+			System.out.println("You Lose...!" +" machine choice is: "+q);
+		
 		else
-			System.out.println("You lose...!"+" machine choice is: "+q);
+			System.out.println("Enter valid option");
 	}
 }
